d9a00ac3c719e1ea59a07a010395b857322e10af
Mustafa Ghnaim <44782684+mut1234@users.noreply.github.com>
2022-06-17 16:04:40 +0300

    Add files via upload

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UsingSecurity.Data;
+
+[assembly: HostingStartup(typeof(UsingSecurity.Areas.Identity.IdentityHostingStartup))]
+namespace UsingSecurity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
